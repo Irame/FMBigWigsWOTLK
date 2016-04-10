a6e2e967908fda1b0d5fc55458302a3581c3bae4
Irame <irame94@gmail.com>
2016-04-10 22:38:11 +0200

    added some plugin utility functions

--- BigWigs_Core/PluginPrototype.lua
+++ BigWigs_Core/PluginPrototype.lua
@@ -23,3 +23,54 @@ end
 
 function plugin:IsBossModule() return end
 
+do
+	local raidList = {}
+	for i = 1, 40 do
+		raidList[i] = format("raid%d", i)
+	end
+	function plugin:GetRaidList()
+		return raidList
+	end
+end
+
+do
+	local partyList = {}
+	partyList[1] = "player"
+	for i = 1, 4 do
+		partyList[i+1] = format("party%d", i)
+	end
+	function plugin:GetPartyList()
+		return partyList
+	end
+end
+
+function plugin:GetGroupList()
+	return GetNumRaidMembers() > 0 and plugin:GetRaidList() or plugin:GetPartyList()
+end
+
+function plugin:GetNumGroupMembers()
+	return GetNumRaidMembers() > 0 and GetNumRaidMembers() or GetNumPartyMembers()+1
+end
+
+function plugin:IsInGroup()
+	return (GetNumRaidMembers() > 0 or GetNumPartyMembers() > 0)
+end
+
+function plugin:IsInRaid()
+	return GetNumRaidMembers() > 0
+end
+
+function plugin:UnitIsGroupOfficer(unit)
+	return UnitIsRaidOfficer(unit) or UnitIsPartyLeader(unit)
+end
+
+function plugin:GetRightChannel(warning)
+	local zoneType = select(2, IsInInstance())
+	if zoneType == "pvp" or zoneType == "arena" then
+		return "BATTLEGROUND"
+	elseif GetRealNumRaidMembers() > 0 then
+		return "RAID"..(warning and "_WARNING" or "")
+	elseif GetRealNumPartyMembers() > 0 then
+		return "PARTY"
+	end
+end
